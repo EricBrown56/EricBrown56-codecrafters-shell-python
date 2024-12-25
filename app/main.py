1f962c3a9c9c0dc4bfbd43df07fbbdb11136f502
EricBrown56 <groove505@gmail.com>
2024-12-25 14:26:59 -0600

    'test'

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ def main():
         
     elif command.startswith("echo"):
         sys.stdout.write(command[5:])
+        sys.stdout.write("$ ")
         
     else:
         sys.stdout.write(f"{command} : command not found")
