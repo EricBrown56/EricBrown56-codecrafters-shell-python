725d8de2e5f61f31dfaa2bdf13c6916701e55cd0
EricBrown56 <groove505@gmail.com>
2024-12-25 14:14:09 -0600

    'test'

--- app/main.py
+++ app/main.py
@@ -4,16 +4,16 @@ import sys
 def main():
     sys.stdout.write("$ ")
     command = input()
-        
-    if command == "exit 0":
-        sys.exit(0)
-           
-    elif command.startswith("echo"):
-        sys.stdout.write(command[5:])
-        sys.stdout.write("\n$ ")
-    else:
-        sys.stdout.write(f"{command} : command not found")
-        sys.stdout.write("\n$ ")
+    while command:    
+        if command == "exit 0":
+            sys.exit(0)
+            
+        elif command.startswith("echo"):
+            sys.stdout.write(command[5:])
+            sys.stdout.write("\n$ ")
+        else:
+            sys.stdout.write(f"{command} : command not found")
+            sys.stdout.write("\n$ ")
         
 
     
