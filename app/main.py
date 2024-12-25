d35f53df17ef193887abbfc135eaaaef32d01018
EricBrown56 <groove505@gmail.com>
2024-12-25 16:08:31 -0600

    'test'

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 import sys
+import os 
 
 
 def main():
     builtins = ["exit", "echo", "type"]
+    PATH = os.environ.get("PATH")
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input() 
+        command_path = None
+        paths = PATH.split(";")
+        for path in paths:
+            if os.path.exists(f"{path}/{command}"):
+                command_path = f"{path}/{command}"
+                
             
         if command == "exit 0":
             sys.exit(0)
@@ -18,7 +26,13 @@ def main():
             if command[5:] in builtins:
                 sys.stdout.write(f"{command[5:]} is a shell builtin\n")
             else:
-                sys.stdout.write(f"{command[5:]}: not found\n")    
+                sys.stdout.write(f"{command[5:]}: not found\n")   
+
+        elif command_path:
+            if command in command_path:
+                sys.stdout.write(f"{command} is {command_path}\n")
+            else:
+                sys.stdout.write(f"{command}: not found\n")
             
         else:
             sys.stdout.write(f"{command}: command not found\n")
